2d28e38cf50b57e912ce7e8f5f38cd258abce61e
Sergey1401 <fire1401@mail.ru>
2024-07-13 10:12:43 +0400

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+#Практическая работа по уроку "Организация программ и методы строк."
+my_string = input()
+print(my_string)
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+#функцию "capitalize()" я добавил к заданию сам, посчитал это интересным.
+print(my_string.capitalize())
+print(my_string.replace(' ',''))
+print(my_string[-1])
